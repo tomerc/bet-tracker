a9ecec0af2a534d6be559f1c9aa48f550e7094bb
arunmarella <arunmarella1108@gmail.com>
2014-06-12 21:34:30 -0400

    Initial commit - 1

--- bet-tracker-api/src/main/java/com/bettracker/domain/BettingSeason.java
+++ bet-tracker-api/src/main/java/com/bettracker/domain/BettingSeason.java
@@ -4,4 +4,13 @@ package com.bettracker.domain;
  * Created by marellas on 6/12/2014.
  */
 public class BettingSeason {
+    private Integer seasonId;
+
+    public Integer getSeasonId() {
+        return seasonId;
+    }
+
+    public void setSeasonId(Integer seasonId) {
+        this.seasonId = seasonId;
+    }
 }

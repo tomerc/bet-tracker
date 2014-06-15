26f8289f4290ad156dcb212a77389a407da94da3
Tomer Cohen <ilan012@gmail.com>
2014-06-15 18:56:50 -0400

    enhance a bit the model module

---
+++ bet-tracker-model/src/main/java/com/bettracker/model/Bet.java
@@ -0,0 +1,10 @@
+package com.bettracker.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Tomer Cohen
+ */
+public class Bet implements Serializable {
+    private static final long serialVersionUID = -6578186565205646125L;
+}

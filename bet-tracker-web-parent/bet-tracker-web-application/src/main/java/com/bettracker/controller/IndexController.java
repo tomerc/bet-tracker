9f938cc238f4995002c3f25745529837ae13cbbf
Tomer Cohen <ilan012@gmail.com>
2014-10-08 19:24:28 -0400

    initial commit of index controller

---
+++ bet-tracker-web-parent/bet-tracker-web-application/src/main/java/com/bettracker/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.bettracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Tomer Cohen
+ */
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String view() {
+        return "index";
+    }
+}
